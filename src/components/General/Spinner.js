d727eec1aecf0c84a75c0059d7c8b79b3f528dd5
Cristina Rueda <crisruedap@gmail.com>
2019-12-04 16:52:04 -0500

    First commit

---
+++ src/components/General/Spinner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../css/spinner.css';
+
+const Spinner = () => (
+    <div className='center'>
+	<div className="lds-default">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>;
+    </div>
+);
+
+export default Spinner;
